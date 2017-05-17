8b7bacef3624f2a2b3d742d2cf738c24de8eedcd
Joe Maples <joe@frap129.org>
2017-05-17 10:34:54 -0500

    d2refreshspr: Carbonize
    Signed-off-by: Joe Maples <joe@frap129.org>

--- carbon_d2refreshspr.mk
+++ carbon_d2refreshspr.mk
@@ -1,13 +1,15 @@
 $(call inherit-product, device/samsung/d2refreshspr/full_d2refreshspr.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/carbon/config/cdma.mk)
+
+PRODUCT_NAME := carbon_d2refreshspr
+PRODUCT_DEVICE := d2refreshspr
+PRODUCT_MANUFACTURER := Samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2refreshspr \
     TARGET_DEVICE=d2refreshspr \
     BUILD_FINGERPRINT="samsung/d2refreshspr/d2refreshspr:4.4.2/KOT49H/L710TVPU0AOH1:user/release-keys" \
     PRIVATE_BUILD_DESC="d2refreshspr-user 4.4.2 KOT49H L710TVPU0AOH1 release-keys"
-
-PRODUCT_NAME := lineage_d2refreshspr
-PRODUCT_DEVICE := d2refreshspr
